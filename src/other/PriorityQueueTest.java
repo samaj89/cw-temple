0a63c672b2ea9145fa56296f620c66a31a0da5e8
janss001 <janss001@gmail.com>
2016-03-28 11:25:08 +0100

    Initial commit; small file with a few tests to establish functionality of PriorityQueueImpl; observed that items in queue are sorted by priority as they are added

---
+++ src/other/PriorityQueueTest.java
@@ -0,0 +1,23 @@
+package other;
+
+import student.PriorityQueue;
+import student.PriorityQueueImpl;
+
+/**
+ * Created by Sam on 28/03/2016.
+ */
+public class PriorityQueueTest {
+
+    public static void main(String[] args) {
+        PriorityQueue<String> pqs = new PriorityQueueImpl<>();
+        pqs.add("First", 5.00);
+        pqs.add("Second", 1.00);
+        pqs.add("Third", 3.00);
+        System.out.println(pqs.toString());
+        System.out.println(pqs.peek());
+        pqs.poll();
+        System.out.println(pqs.toString());
+        pqs.updatePriority("First", 2.00);
+        System.out.println(pqs.toString());
+    }
+}
